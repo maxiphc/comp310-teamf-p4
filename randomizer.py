78c7642052945a3433d4e9e991010f6d7f36041f
Max Ip <hip@sandiego.edu>
2022-05-09 16:46:10 -0700

    modified random function to return integer values

--- randomizer.py
+++ randomizer.py
@@ -1,9 +1,11 @@
 import time
+import math
 
 class PseudoRandom:
     
     def __init__(self):
         self.seed = -1
+        self.prev = 0
         self.a = 25214903917
         self.c = 11
         self.m = 2**31
@@ -12,23 +14,22 @@ class PseudoRandom:
         seed = time.monotonic() 
         self.seed = int(str(seed)[-3:])  # taking the 3 decimal places at the end of what is returned by time.monotonic()
 
-    def generate_random(self, prev_random):
+    def generate_random(self, prev_random, range):
         """
-        Generate random numbers 
+        Returns a pseudorandom number between 1 and range.
         """
         # if first value, then get the seed to determine starting point
         if self.seed == -1:
             self.get_seed()
-            return (self.a * self.seed + self.c) % self.m
-            # return ( (self.a * self.seed + self.c) % self.m ) / self.m  # use for random percent 
+            self.prev = raw_num = (self.a * self.seed + self.c) % self.m
         # use previous value to determine next number
         else:
-            return (self.a * prev_random + self.c) % self.m
-            # return ( (self.a * prev_random + self.c) % self.m ) / self.m  # use for random percent
+            self.prev = raw_num = (self.a * prev_random + self.c) % self.m
+        
+        return math.ceil((raw_num / self.m) * range)
 
 if __name__ == "__main__":
     test = PseudoRandom()
-    rand = 0
     for i in range(10):
-        rand = test.generate_random(rand)
-        print(rand / test.m)
+        rand = test.generate_random(test.prev, 10)
+        print(rand)
